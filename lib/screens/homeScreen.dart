068b143670dd6773b399d7f3c74762545788beb9
Betrayer <StarDestroyer@ukr.net>
2020-08-06 12:42:37 +0300

    quiz done

---
+++ lib/screens/homeScreen.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_tests/models/Question.dart';
+// import 'package:flutter_tests/widgets/answer.dart';
+import 'package:flutter_tests/widgets/progress_bar.dart';
+import 'package:flutter_tests/widgets/quiz.dart';
+import 'package:flutter_tests/widgets/result.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final QuestionData data = QuestionData();
+  int _countResult = 0;
+  int _questionIndex = 0;
+  List<Icon> _icons = [];
+  void _clearState() => setState(() {
+        _questionIndex = 0;
+        _countResult = 0;
+        _icons = [];
+      });
+
+  void _onChangeAnswer(bool isCorrect) => setState(() {
+        if (isCorrect) {
+          _icons.add(Icon(Icons.face, color: Color(0xFFbd27ff)));
+          _countResult++;
+        } else {
+          _icons.add(Icon(Icons.face, color: Color(0xFF000000)));
+        }
+        _questionIndex += 1;
+      });
+
+  final whiteTextStyle = TextStyle(
+    color: Colors.white,
+    fontSize: 22,
+  );
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // appBar: AppBar(
+      // title: Text("Under test"),
+      // ),
+      body: SafeArea(
+        child: DefaultTextStyle.merge(
+          style: whiteTextStyle,
+          child: Container(
+            constraints: BoxConstraints.expand(),
+            decoration: BoxDecoration(
+              color: const Color(0xff2a375a),
+              image: DecorationImage(
+                image: AssetImage('assets/images/another_bg_test.jpg'),
+                fit: BoxFit.cover,
+              ),
+            ),
+            child: Column(
+              children: <Widget>[
+                ProgressBar(
+                  icons: _icons,
+                  count: _questionIndex,
+                  total: data.questions.length,
+                ),
+                _questionIndex < data.questions.length
+                    ? Quiz(
+                        index: _questionIndex,
+                        questionData: data,
+                        onChangeAnswer: _onChangeAnswer,
+                      )
+                    : Result(
+                        count: _countResult,
+                        total: data.questions.length,
+                        onClearState: _clearState,
+                      )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
